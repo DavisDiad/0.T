be5ee08909fa6e673db35a9a4fc4895e044f4886
DavisDiad <40240480@esmad.ipp.pt>
2026-01-08 16:31:51 +0000

    Addition of Ecila's portrait and sprite.

--- scripts/UI/UI.gd
+++ scripts/UI/UI.gd
@@ -2,10 +2,21 @@ extends CanvasLayer
 
 @onready var portrait: TextureRect = $ControlPortrait/Portrait
 @onready var control_buttons: Control = $ControlButtons
+@onready var control_options: Control = $ControlOptions
 @onready var text_ui: TextureRect = $ControlUI/TextUI
 @onready var text: Control = $Text
 
 @onready var character_1: Node3D = $"../Character1"
+
+@onready var speak_texture: TextureButton = $ControlButtons/SpeakTexture
+
+
+func _ready() -> void:
+	portrait.visible = false
+	text_ui.visible = false
+	control_buttons.visible = false
+	control_options.visible = false
+	text.visible = false
 
 
 func _on_bunker_show_options() -> void:
@@ -23,3 +34,20 @@ func _on_return_button_pressed() -> void:
 		control_buttons.visible = false
 		text.visible = false
 		character_1.visible = true
+	elif portrait.visible == true and control_buttons.visible == false and control_options.visible == true:
+		control_buttons.visible = true
+		control_options.visible = false
+
+
+func _on_speak_button_pressed() -> void:
+	var timer: Timer = Timer.new()
+	add_child(timer)
+	timer.one_shot = true
+	timer.autostart = false
+	timer.wait_time = 0.1
+	timer.timeout.connect(_timer_Timeout)
+	timer.start()
+
+func _timer_Timeout():
+	control_buttons.visible = false
+	control_options.visible = true
--- scripts/bunker/bunker.gd
+++ scripts/bunker/bunker.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 var hovering = false
 
+var pollution_meter = 35
+
 signal show_options
 
 # Called when the node enters the scene tree for the first time.
@@ -9,7 +11,9 @@ func _ready() -> void:
 	pass
 
 func _process(_delta: float) -> void:
-	pass
+	print (pollution_meter)
+	if pollution_meter == 100 or pollution_meter > 100:
+		print ("game over")
 
 
 func _on_character1_mouse_entered() -> void:
@@ -21,6 +25,7 @@ func _on_character1_mouse_exited() -> void:
 func _on_character1_input_event(_camera: Node, _event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	if hovering == true and Input.is_action_just_pressed("left_click"):
 		show_options.emit()
+		pollution_meter += 1
 
 
 func _on_character2_mouse_entered() -> void:
