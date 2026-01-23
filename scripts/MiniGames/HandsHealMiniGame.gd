c3c314bdd6ddce8179fab66ec7b64bf8e0937af4
DavisDiad <40240480@esmad.ipp.pt>
2026-01-23 00:45:42 +0000

    added healing mini game

--- scripts/MiniGames/HandsHealMiniGame.gd
+++ scripts/MiniGames/HandsHealMiniGame.gd
@@ -2,13 +2,39 @@ extends Control
 
 var hover = false
 
+var completed = false
+var fill_time := 2.0
+var hold_time := 0.0
+var start_color := Color.WHITE
+var target_color := Color.LIGHT_GREEN
 
 func _ready() -> void:
 	$HealMouse.connect("mouse_entered", can_click)
 	$HealMouse.connect("mouse_exited", cant_click)
+	
+	start_color.a8 = 0
 
 
 func can_click() -> void:
 	hover = true
 func cant_click() -> void:
 	hover = false
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("left_click") and hover and $HealedHand.visible:
+		hold_time += delta
+	else:
+		hold_time -= delta
+	hold_time = clamp(hold_time, 0.0, fill_time)
+	var t := hold_time / fill_time
+	$HealedHand.modulate = start_color.lerp(target_color, t)
+	if hold_time >= fill_time:
+		healed()
+func healed() -> void:
+		$HealAnimation.visible = true
+		$HealAnimation.play()
+		await get_tree().create_timer(1.0).timeout
+		$HealAnimation.stop()
+		hover = false
+		completed = false
+		$HealAnimation.visible = false
+		$HealedHand.visible = false
